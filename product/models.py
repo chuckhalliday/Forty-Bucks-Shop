af061191b2abe3e9a0a1d6ec6dd8d0e4a27fa43d
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-09-17 16:25:16 -0700

    Change product model

--- product/models.py
+++ product/models.py
@@ -38,18 +38,18 @@ class Product(models.Model):
 
     def get_image(self):
         if self.image:
-            return 'fortybucks.herokuapp.com' + self.image.url
+            return 'https://fortybucks.herokuapp.com' + self.image.url
         return ''
 
     def get_thumbnail(self):
         if self.thumbnail:
-            return 'fortybucks.herokuapp.com' + self.thumbnail.url
+            return 'https://fortybucks.herokuapp.com' + self.thumbnail.url
         else:
             if self.image:
                 self.thumbnail = self.make_thumbnail(self.image)
                 self.save()
 
-                return 'fortybucks.herokuapp.com' + self.thumbnail.url
+                return 'https://fortybucks.herokuapp.com' + self.thumbnail.url
             else:
                 return ''
 
