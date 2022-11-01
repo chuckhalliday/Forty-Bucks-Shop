a93a73db0e3d396d1016fd7330107e6808e2ce71
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-10-31 19:51:28 -0700

    Update views.py

--- order/views.py
+++ order/views.py
@@ -21,7 +21,7 @@ def checkout(request):
 
     if serializer.is_valid():
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
+        paid_amount = sum(item.get('quantity') * item.get('price') for item in serializer.validated_data['items'])
 
         try:
             charge = stripe.Charge.create(
