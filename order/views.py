0599b7c2657693073b750d32c15bbcdf2721c856
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-11-06 20:51:48 -0800

    Update views.py

--- order/views.py
+++ order/views.py
@@ -1,4 +1,6 @@
 import stripe
+import os
+import environ
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -13,6 +15,8 @@ from rest_framework.response import Response
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, MyOrderSerializer
 
+STRIPE_SECRET_KEY = (os.environ.get("STRIPE_SECRET_KEY", 'dev default value'))
+
 @api_view(['POST'])
 @authentication_classes([authentication.TokenAuthentication])
 @permission_classes([permissions.IsAuthenticated])
@@ -20,7 +24,7 @@ def checkout(request):
     serializer = OrderSerializer(data=request.data)
 
     if serializer.is_valid():
-        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.api_key = STRIPE_SECRET_KEY
         paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
 
         try:
