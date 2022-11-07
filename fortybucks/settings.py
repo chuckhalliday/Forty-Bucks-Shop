8a84918eb01c489dae7eee08c1b2116f03d95b44
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-11-06 21:00:50 -0800

    Update settings.py

--- fortybucks/settings.py
+++ fortybucks/settings.py
@@ -51,6 +51,10 @@ CORS_ALLOWED_ORIGINS = [
     "https://fortybucks.herokuapp.com"
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://www.givemefortybucks.com"
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
