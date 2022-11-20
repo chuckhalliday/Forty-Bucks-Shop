a1f84e2ec4785d9db90693a2ea12f311e3bfb8a2
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-11-19 21:10:42 -0800

    Update settings.py

--- fortybucks/settings.py
+++ fortybucks/settings.py
@@ -24,6 +24,20 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'fortybucks.herokuapp.com', 'drive.google.com']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 
 
 # Application definition
