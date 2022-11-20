ad0cfe39a2c1f0a6c4a72cdfabfe4751837cde03
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-11-19 21:44:50 -0800

    Update Checkout.vue

--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -190,7 +190,7 @@ export default {
                 const obj = {
                     product: item.product.id,
                     quantity: item.quantity,
-                    price: (item.product.price * item.quantity)
+                    price: (item.product.price * item.quantity).toFixed(2)
                 }
 
                 items.push(obj)
